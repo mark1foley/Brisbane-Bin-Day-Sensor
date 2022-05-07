1e216fc960ca9b7bac1c326d61c88f0cf687e323
mark1foley <50077728+mark1foley@users.noreply.github.com>
2022-05-07 12:54:34 +1000

    Update sensor.py

--- custom_components/bne_wc/sensor.py
+++ custom_components/bne_wc/sensor.py
@@ -1,8 +1,3 @@
-# Waste Collection Algorithm 
-# Explanation:
-# If the ZONE for the address matches the ZONE for the week, it is yellow recycling bin week.
-# If the ZONE for the address does not match the ZONE for the week, it is green waste bin week.
-
 import math
 import requests
 import logging
@@ -13,11 +8,9 @@ from dateutil.parser import parse
 from calendar import weekday
 from datetime import datetime, timedelta, date
 from time import strptime
-#from geopy.geocoders import Nominatim
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (CONF_NAME)
-#import homeassistant.util.dt as dt_util
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 import homeassistant.helpers.config_validation as cv
@@ -25,18 +18,25 @@ import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_PROPERTY_NUMBER = "Property Number"
+ATTR_SUBURB = "Suburb"
+ATTR_STREET = "Street"
+ATTR_HOUSE_NUMBER = "House Number"
 ATTR_COLLECTION_DAY = "Collection Day"
 ATTR_COLLECTION_ZONE = "Collection Zone"
 ATTR_NEXT_COLLECTION_DATE = "Next Collection Date"
 ATTR_ICON = "Icon"
+ATTR_ALERT_HOURS = "Alert Hours"
+ATTR_EXTRA_BIN = "Extra Bin"
 
 CONF_BASE_URL = 'base_url'
 CONF_WASTE_DAYS_TABLE = 'days_table'
 CONF_WASTE_WEEKS_TABLE = 'weeks_table'
 CONF_PROPERTY_NUMBER = 'property_number'
 CONF_ICON = 'icon'
+CONF_ALERT_HOURS = 'alert_hours'
 
 DEFAULT_ICON = 'mdi:trash-can'
+DEFAULT_ALERT_HOURS = 12
 
 #MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=3600)
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=60)
@@ -55,28 +55,24 @@ def is_valid_date(date):
 
 def due_in_hours(time_stamp: datetime):
     """Get the remaining hours from now until a given datetime object."""
-    _LOGGER.debug("due_in_hour calc")
-    
-    diff = parse(time_stamp) - datetime.now()
+    diff = time_stamp - datetime.now()
     return math.ceil(diff.seconds/3660) + (diff.days*DAY_HOURS)
 
 def date_today():
     return datetime.combine(date.today(), datetime.min.time())
     
-def week_day(timestamp):
-    return weekday(timestamp.year, timestamp.month, timestamp.day)
+def week_day():
+    return datetime.today().weekday()
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
     vol.Required(CONF_BASE_URL): cv.string,
     vol.Required(CONF_WASTE_DAYS_TABLE): cv.string,
     vol.Required(CONF_WASTE_WEEKS_TABLE): cv.string,
-    vol.Required(CONF_PROPERTY_NUMBER): cv.string,
+    vol.Required(CONF_PROPERTY_NUMBER): cv.positive_int,
+    vol.Optional(CONF_ALERT_HOURS, default=DEFAULT_ALERT_HOURS): cv.positive_int,
     vol.Optional(CONF_ICON, default=DEFAULT_ICON): cv.string
 })
-
-#weekStartDate = date_today - timedelta(date_today.weekday())
-#weekStartString = f'{weekStartDate.day}/{weekStartDate:%m/%Y}'
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Get the waste collection sensor."""
@@ -87,82 +83,28 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         data, 
         config.get(CONF_NAME),
         config.get(CONF_ICON),
-        config.get(CONF_PROPERTY_NUMBER)
+        config.get(CONF_PROPERTY_NUMBER),
+        config.get(CONF_ALERT_HOURS)
     ))
 
     add_devices(sensors)
 
-
-#geolocator = Nominatim(user_agent='HA_BNE_Waste_Collection')
-#location = geolocator.reverse("-27.51753948915311, 153.01444530487063")
-#address = location.raw['address']
-#houseNumber = address.get('house_number', '')
-#houseRoad = address.get('road', '')[0:address.get('road', '').rfind(" ")]
-#houseSuburb = address.get('suburb', '')
-#print(BASE_URL + WASTE_DAYS_TABLE + '&q={"HOUSE_NUMBER":"' + houseNumber + '","STREET_NAME":"' + houseRoad + '","SUBURB":"' + houseSuburb + '"}')
-
-# Make the HTTP request
-#try:  
-#    # response = requests.get(BASE_URL + WASTE_DAYS_TABLE + '&q={"PROPERTY_NUMBER":"' + propertyId + '"}')
-#    response = requests.get(BASE_URL + WASTE_DAYS_TABLE + '&q={"HOUSE_NUMBER":"' + houseNumber + '","STREET_NAME":"' + houseRoad + '","SUBURB":"' + houseSuburb + '"}')
-#    json=response.json()
-#    if json['success'] == True:
-#        dic=json['result']
-#        df = pd.DataFrame(dic['records'])
-#        if len(df.index) > 0:
-#            coll_day = df['COLLECTION_DAY'].iloc[0]
-#            coll_zone = df['ZONE'].iloc[0]
-#            # Dataset contains week day names so need to convert it into integer for the day
-#            coll_day_no = strptime(coll_day,'%A').tm_wday
-#            if coll_day_no > weekDayNo:
-#                next_coll_date = dateToday + timedelta(days=coll_day_no-weekDayNo)
-#            else:
-#                next_coll_date = dateToday + timedelta(days=(WEEK_DAYS+coll_day_no)-weekDayNo)
-#            time_to_coll = next_coll_date - datetime.now()
-#            hours_to_coll = math.ceil(time_to_coll.seconds/3660) + (time_to_coll.days*DAY_HOURS)
-#            try:
-#                response = requests.get(BASE_URL + WASTE_WEEKS_TABLE + '&q={"WEEK_STARTING":"' + weekStartString + ',"ZONE":' + coll_zone + '"}')
-#                json=response.json()
-#                if json['success'] == True:
-#                    dic=json['result']
-#                    df = pd.DataFrame(dic['records'])
-#                    if len(df.index) > 0:
-#                        additionalBin = 'Yellow'
-#                    else:
-#                        additionalBin = 'Green'
-#                    print('Address: ' + houseNumber + ' ' + houseRoad + ' ' + houseSuburb)
-#                    print('Collection Day: ' + coll_day)
-#                    print('Next Collection Date: ' + f'{next_coll_date}')
-#                    print('Additional Bin: ' + f'{additionalBin}')
-#                    print('Hours To Collection: ' + f'{hours_to_coll}')
-#                else:
-#                    print('Weeks Dataset error')
-#            except requests.exceptions.RequestException as e:
-#                print(e)
-#        else:
-#            print('Zero rows returned')
-#    else:
-#        print('Days Dataset error')
-#except requests.exceptions.RequestException as e:
-#    print(e)
-
 class BneWasteCollectionSensor(Entity):
     """Implementation of a waste collection sensor."""
 
-    def __init__(self, data, name, icon, property_number):
+    def __init__(self, data, name, icon, property_number, alert_hours):
         """Initialize the sensor."""
         self.data = data
         self._name = name
         self._icon = icon
         self._property_number = property_number
+        self._alert_hours = alert_hours
         self.update()
 
     def _get_collection_details(self):
         collection_details = {}
         for key, value in self.data.info.items():
             collection_details[key] = value
-            
-        _LOGGER.debug("Collection Details: {0}".format(collection_details))
         return collection_details
 
     @property
@@ -182,9 +124,14 @@ class BneWasteCollectionSensor(Entity):
         attrs = {
             ATTR_PROPERTY_NUMBER: self._property_number,
             ATTR_ICON: self._icon,
+            ATTR_ALERT_HOURS: self._alert_hours,
+            ATTR_SUBURB: collection_details[ATTR_SUBURB],
+            ATTR_STREET: collection_details[ATTR_STREET],
+            ATTR_HOUSE_NUMBER: collection_details[ATTR_HOUSE_NUMBER],
             ATTR_COLLECTION_DAY: collection_details[ATTR_COLLECTION_DAY],
             ATTR_COLLECTION_ZONE: collection_details[ATTR_COLLECTION_ZONE],
-            ATTR_NEXT_COLLECTION_DATE: collection_details[ATTR_NEXT_COLLECTION_DATE]
+            ATTR_NEXT_COLLECTION_DATE: collection_details[ATTR_NEXT_COLLECTION_DATE],
+            ATTR_EXTRA_BIN: collection_details[ATTR_EXTRA_BIN]
         }
 
         return attrs
@@ -210,6 +157,18 @@ class BneWasteCollectionSensor(Entity):
         except:
             _LOGGER.debug("...{0} not defined".format(ATTR_PROPERTY_NUMBER))
         try:
+            _LOGGER.debug("...{0}: {1}".format(ATTR_SUBURB,self.extra_state_attributes[ATTR_SUBURB]))
+        except:
+            _LOGGER.debug("...{0} not defined".format(ATTR_SUBURB))
+        try:
+            _LOGGER.debug("...{0}: {1}".format(ATTR_STREET,self.extra_state_attributes[ATTR_STREET]))
+        except:
+            _LOGGER.debug("...{0} not defined".format(ATTR_STREET))
+        try:
+            _LOGGER.debug("...{0}: {1}".format(ATTR_HOUSE_NUMBER,self.extra_state_attributes[ATTR_HOUSE_NUMBER]))
+        except:
+            _LOGGER.debug("...{0} not defined".format(ATTR_HOUSE_NUMBER))
+        try:
             _LOGGER.debug("...{0}: {1}".format(ATTR_COLLECTION_DAY,self.extra_state_attributes[ATTR_COLLECTION_DAY]))
         except:
             _LOGGER.debug("...{0} not defined".format(ATTR_COLLECTION_DAY))
@@ -221,6 +180,14 @@ class BneWasteCollectionSensor(Entity):
             _LOGGER.debug("...{0}: {1}".format(ATTR_NEXT_COLLECTION_DATE,self.extra_state_attributes[ATTR_COLLECTION_ZONE]))
         except:
             _LOGGER.debug("...{0} not defined".format(ATTR_NEXT_COLLECTION_DATE))
+        try:
+            _LOGGER.debug("...{0}: {1}".format(ATTR_EXTRA_BIN,self.extra_state_attributes[ATTR_EXTRA_BIN]))
+        except:
+            _LOGGER.debug("...{0} not defined".format(ATTR_EXTRA_BIN))
+        try:
+            _LOGGER.debug("...{0}: {1}".format(ATTR_ALERT_HOURS,self.extra_state_attributes[ATTR_ALERT_HOURS]))
+        except:
+            _LOGGER.debug("...{0} not defined".format(ATTR_ALERT_HOURS))
 
 class BneWasteCollection(object):
     """The Class for handling the data retrieval."""
@@ -236,21 +203,25 @@ class BneWasteCollection(object):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
 
-        self.info = self._get_collection_details() if self._property_number else {}
+        collection = self._get_collection_details() if self._property_number else {}
+        self._get_extra_bin(collection)
 
     def _get_collection_details(self):
 
         collection = {}
         try:
             collection[ATTR_PROPERTY_NUMBER] = self._property_number
-            _LOGGER.debug("query: {0}{1}{2}{3}{4}".format(self._base_url,self._days_table,'&q={"PROPERTY_NUMBER":"',self._property_number, '"}'))
-            response = requests.get(self._base_url + self._days_table + '&q={"PROPERTY_NUMBER":"' + self._property_number + '"}')
+            _LOGGER.debug("...Day query: {0}{1}{2}{3}{4}".format(self._base_url,self._days_table,'&q={"PROPERTY_NUMBER":"',self._property_number, '"}'))
+            response = requests.get(self._base_url + self._days_table + '&q={"PROPERTY_NUMBER":"' + str(self._property_number) + '"}')
             json=response.json()
             if json['success'] == True:
                 _LOGGER.info("Successfully retrieved collection day dataset")
                 dic=json['result']
                 df = pd.DataFrame(dic['records'])
                 if len(df.index) > 0:
+                    collection[ATTR_SUBURB] = df['SUBURB'].iloc[0]
+                    collection[ATTR_STREET] = df['STREET_NAME'].iloc[0]
+                    collection[ATTR_HOUSE_NUMBER] = df['HOUSE_NUMBER'].iloc[0]
                     collection[ATTR_COLLECTION_DAY] = df['COLLECTION_DAY'].iloc[0]
                     collection[ATTR_COLLECTION_ZONE] = df['ZONE'].iloc[0]
 
@@ -270,3 +241,28 @@ class BneWasteCollection(object):
                 
         return collection
 
+    def _get_extra_bin(self, collection):
+        # Waste Collection Algorithm 
+        # Explanation:
+        # If the ZONE for the address matches the ZONE for the week, it is yellow recycling bin week.
+        # If the ZONE for the address does not match the ZONE for the week, it is green waste bin week.
+        weekStartDate = date_today() - timedelta(days=week_day())
+        weekStartString = f'{weekStartDate.day}/{weekStartDate:%m/%Y}'
+
+        try:
+            _LOGGER.debug("...Week query: {0}{1}{2}{3}{4}{5}{6}".format(self._base_url,self._weeks_table,'&q={"WEEK_STARTING":"',weekStartString,',"ZONE":',collection[ATTR_COLLECTION_ZONE], '"}'))
+            response = requests.get(self._base_url + self._weeks_table + '&q={"WEEK_STARTING":"' + weekStartString + ',"ZONE":' + collection[ATTR_COLLECTION_ZONE] + '"}')
+            json=response.json()
+            if json['success'] == True:
+                dic=json['result']
+                df = pd.DataFrame(dic['records'])
+                if len(df.index) > 0:
+                    collection[ATTR_EXTRA_BIN] = 'Yellow/Recycling'
+                else:
+                    collection[ATTR_EXTRA_BIN] = 'Green/Garden'
+            else:
+                _LOGGER.error("Error retrieving collection weeks dataset")
+        except requests.exceptions.RequestException as e:
+            _LOGGER.error("updating collection week got {}.".format(requests.exceptions.RequestException))
+
+        self.info = collection
